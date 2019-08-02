eb46bd346bd19cc54c8937be6ec353c3b043a8b2
surmon-china <surmon@foxmail.com>
2019-08-02 15:35:42 +0800

    remove winXP system

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
 import { MacOS } from './systems/macOS';
 import { Win95 } from './systems/win95';
-import { WinXP } from './systems/winXP';
 import { Windows } from './systems/windows';
 
 import 'normalize.css';
@@ -12,7 +11,6 @@ import './styles/os.scss';
 
 export const App: React.FC = observer(() => {
   // return <Win95 />
-  // return <WinXP />
   // return <Windows />
   return <MacOS />
 })
